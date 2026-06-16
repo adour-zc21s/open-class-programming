850b38aae939f10db135b33c157f7727fb29adf5
adour-zc21s <wahid.asimetris@gmail.com>
2026-06-16 22:04:08 +0700

    change comment

--- src/main/java/com/adour/openclassprog/config/mapper/ModelMapperConfig.java
+++ src/main/java/com/adour/openclassprog/config/mapper/ModelMapperConfig.java
@@ -1,5 +1,8 @@
 package com.adour.openclassprog.config.mapper;
 
+import com.adour.openclassprog.dto.CrmDTO;
+import com.adour.openclassprog.model.Crm;
+import org.modelmapper.Converter;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,6 +21,12 @@ public class ModelMapperConfig {
         // KONFIGURASI PENTING: Mengabaikan field yang bernilai null saat mapping
         modelMapper.getConfiguration()
                 .setSkipNullEnabled(true);
+        // Konverter string menjadi huruf besar.
+        Converter<String, String> toUppercase = ctx ->
+                ctx.getSource() != null ? ctx.getSource().toUpperCase() : null;
+        modelMapper.typeMap(CrmDTO.class, Crm.class).addMappings(mapper -> {
+            mapper.using(toUppercase).map(CrmDTO::getName, Crm::setName);
+        });
         return modelMapper;
     }
 }
